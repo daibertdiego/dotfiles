8a0d7a1285c18b2795ca461e0f5a95c2333aed27
Diego Daibert <daibertdiego@gmail.com>
2025-01-27 23:10:28 -0600

    Fixing dot repeatable commands.

--- .config/nvim/lua/plugins/web-tools.lua
+++ .config/nvim/lua/plugins/web-tools.lua
@@ -1,16 +1,16 @@
-require 'web-tools'.setup({
-  keymaps = {
-    rename = nil,        -- by default use same setup of lspconfig
-    repeat_rename = '.', -- . to repeat
-  },
-  hurl = {
-    -- hurl default
-    show_headers = false, -- do not show http headers
-    floating = false,     -- use floating windows (need guihua.lua)
-    formatters = {
-      -- format the result by filetype
-      json = { 'jq' },
-      html = { 'prettier', '--parser', 'html' },
-    },
-  },
+require("web-tools").setup({
+	keymaps = {
+		rename = nil, -- by default use same setup of lspconfig
+		repeat_rename = "nil", -- . to repeat
+	},
+	hurl = {
+		-- hurl default
+		show_headers = false, -- do not show http headers
+		floating = false, -- use floating windows (need guihua.lua)
+		formatters = {
+			-- format the result by filetype
+			json = { "jq" },
+			html = { "prettier", "--parser", "html" },
+		},
+	},
 })
